0b63b1aa1243a552d697ee3db63d50b21f88156e
Meind <105383135+WMeindW@users.noreply.github.com>
2023-04-05 13:47:07 +0200

    UI-Layout

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,11 +2,12 @@ import "./Message.css"
 
 export default function Message() {
     return <div>
-        <div className="msg-container">
+        <div className="msg-container text-light">
+            <div className = "inner-container bg-dark"></div>
         </div>
-        <div className="usr-container">
+        <div className="usr-container text-light">
         </div>
-        <div className="msc-container">
+        <div className="msc-container text-light">
         </div>
     </div>;
 }
