459468f0501ecff178cd2a3240d4d4148b9fb700
WMeindW <daniellinda2005@gmail.com>
2023-04-05 22:30:28 +0200

    Message.css

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,13 +1,15 @@
-import "./Message.css"
+import "./Message.css";
 
 export default function Message() {
     return <div>
         <div className="msg-container text-light">
-            <div className = "inner-container bg-dark"></div>
+            <div className="inner-container-msg bg-dark"></div>
         </div>
         <div className="usr-container text-light">
+            <div className="inner-container-usr bg-dark"></div>
         </div>
         <div className="msc-container text-light">
+            <div className="inner-container-msc bg-dark"></div>
         </div>
     </div>;
 }
