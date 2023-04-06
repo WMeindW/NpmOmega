722eeafd277d9dc3dc9283a2d51286230764fd4c
WMeindW <daniellinda2005@gmail.com>
2023-04-06 14:59:26 +0200

    Message.css

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,10 +6,29 @@ export default function Message() {
             <div className="inner-container-msg bg-dark"></div>
         </div>
         <div className="usr-container text-light">
-            <div className="inner-container-usr bg-dark"></div>
+            <div className="inner-container-usr bg-dark">
+                <div className="row usr-row mx-auto">
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                    <div className="card usr-card mx-auto"></div>
+                </div>
+            </div>
         </div>
         <div className="msc-container text-light">
-            <div className="inner-container-msc bg-dark"></div>
+            <div className="inner-container-msc bg-dark">
+                <div className="row msc-row mx-auto">
+                    <div className="card msc-card mx-auto"></div>
+                </div>
+            </div>
         </div>
     </div>;
 }
