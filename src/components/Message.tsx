2196f7375eadd4ce2fe72346899b14be565b76a6
WMeindW <daniellinda2005@gmail.com>
2023-04-08 20:29:35 +0200

    Message.css

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,7 +3,22 @@ import "./Message.css";
 export default function Message() {
     return <div>
         <div className="msg-container text-light">
-            <div className="inner-container-msg bg-dark"></div>
+            <div className="inner-container-msg bg-dark">
+                <div className="msg-row mx-auto">
+                    <div className="message">
+                        <span className="sent">Nazdaaaaaaaaaaar</span>
+                    </div>
+                    <div className="message">
+                        <span className="received">Ahojjjjjjjjjjjjjjjj</span>
+                    </div>
+                </div>
+                <label className="input-label mx-auto">
+                    <form method="post" action="/send">
+                        <input type="text" className="input-text text-light"/>
+                        <input type="submit" value="Send" className="input-button text-light"/>
+                    </form>
+                </label>
+            </div>
         </div>
         <div className="usr-container text-light">
             <div className="inner-container-usr bg-dark">
