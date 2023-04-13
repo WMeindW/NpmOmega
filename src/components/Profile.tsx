41aff95ce93d0b610ccb87f469a56f4ace333337
WMeindW <daniellinda2005@gmail.com>
2023-04-13 16:59:48 +0200

    Profile.css

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,34 @@
 import "./Profile.css";
 
 export default function Profile() {
-    return <p className="text-light">Profile</p>;
+    return (<form action="/edit" method="post">
+        <div className="picture-container">
+            <img className="picture bg-dark" src="https://www.pngmart.com/files/21/Account-Avatar-Profile-PNG-Photo.png"
+                 alt="profile.png"/>
+            <button type="submit" className="action-button btn btn-primary">Save</button>
+            <button formAction="/logout" className="action-button btn btn-danger">Logout</button>
+        </div>
+        <div className="info-container">
+            <div className="info-section">
+                <p className="info-section-heading bg-dark text-light">Name</p>
+                <input className="info-section-text bg-dark text-light" type="text"/>
+                <div className="divider bg-dark"></div>
+            </div>
+            <div className="info-section">
+                <p className="info-section-heading bg-dark text-light">Name</p>
+                <input className="info-section-text bg-dark text-light" type="text"/>
+                <div className="divider bg-dark"></div>
+            </div>
+            <div className="info-section">
+                <p className="info-section-heading bg-dark text-light">Name</p>
+                <input className="info-section-text bg-dark text-light" type="text"/>
+                <div className="divider bg-dark"></div>
+            </div>
+            <div className="info-section">
+                <p className="info-section-heading bg-dark text-light">Name</p>
+                <input className="info-section-text bg-dark text-light" type="text"/>
+                <div className="divider bg-dark"></div>
+            </div>
+        </div>
+    </form>);
 }
